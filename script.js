c077066421be2dcbc775781a4bc2f0ed89989c1b
tgochitashvili <tazo.gochitashvili@gmail.com>
2020-07-26 17:26:39 +0400

    Added more functionality, columns

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
-    let numAgents = Math.ceil(Math.random()*25);
-    let names = [];
+    let numAgents = Math.ceil(Math.random()*50);
     $.when(
         $.getJSON("assets/firstnames.json"),
         $.getJSON("assets/lastnames.json")
     ).done(function(fnames, lnames) {
+        let departments = [
+            "Customer Service",
+            "Sales",
+            "HR Department",
+            "PR Department"
+        ];
         let fullnames = [];
         for (var i = 0; i < numAgents; i++)
             fullnames.push(randomChoice(fnames[0]) + " " + randomChoice(lnames[0]));
+        let tbody = $("table").find("tbody");
+
+        for(var name of fullnames){
+            var rand = Math.round(Math.random());
+            var status = rand?"active":"inactive";
+            var agent=[
+                "<td><img class='avatar' src='https://picsum.photos/200?"+Math.floor(Math.random()*1000)+"'></td>",
+                "<td>" + name + "</td>",
+                "<td class='status'>" + (rand?"Online":"Offline") + "</td>",
+                "<td>" + randomChoice(departments) + "</td>",
+                "<td class='serving'>Serving " + Math.floor(Math.random()*15) + "</td>" 
+            ];
+            tbody.append("<tr class = '"+ status + "'>" + agent + "</tr>");
+        }
         
+
+        function TD(text){
+
+        }
         function randomChoice(data){
             return data[Math.floor(Math.random()*data.length)];
-        }
-        function generateTable(table, data) {
-            for (let element of data) {
-                let row = table.insertRow();
-                let cell = row.insertCell();
-                let text = document.createTextNode(element);
-                cell.appendChild(text);
-            }
-        }
-        let table = document.querySelector("table");
-        generateTable(table, fullnames);
-        
+        }       
     });
 
 });
@@ -32,14 +44,14 @@ function searchAgents(element){
     for(let i = 0; i < tr.length; i++){
         let currentTR = $(tr[i]);
         let td = currentTR.find("td");
-        let tdContent = td[0].innerText.toLowerCase()
+        let tdContent = td[1].innerText.toLowerCase()
         if(tdContent.includes(input)){
             td.slideDown(400);
-            currentTR.slideDown(100);
+            currentTR.slideDown(250);
         }
         else{
             td.slideUp(400);
-            currentTR.slideUp(100);
+            currentTR.slideUp(250);
         }
     }
 }
